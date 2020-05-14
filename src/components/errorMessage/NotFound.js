ceb2c003d7c64233a1374fe0265bb20e229eacfd
Katrusy86 <katrin.myronova@gmail.com>
2020-05-14 22:26:22 +0300

    Fixed bugs

--- src/components/errorMessage/NotFound.js
+++ src/components/errorMessage/NotFound.js
@@ -3,7 +3,7 @@ import React from 'react';
 const NotFound = () => (
     <h2>There are no reviews for this movie
     <span role="img" arial-label="emoji">
-        ;)
+        😏
     </span>
     </h2>
 );
